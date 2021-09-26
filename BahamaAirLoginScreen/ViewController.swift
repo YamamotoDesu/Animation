987d4d32daf9d0684f5fe869c28018569e966d39
yamamoto <kyoyamamoto1202@gmail.com>
2021-09-26 12:42:24 +0900

    adding spinner gif

--- BahamaAirLoginScreen/ViewController.swift
+++ BahamaAirLoginScreen/ViewController.swift
@@ -174,6 +174,19 @@ class ViewController: UIViewController {
     
     @IBAction func login() {
         view.endEditing(true)
+        UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: {
+            self.loginButton.bounds.size.width += 80.0
+            
+            self.loginButton.backgroundColor =
+            UIColor(red: 0.85, green: 0.83, blue: 0.45, alpha: 1.0)
+            
+            self.spinner.center = CGPoint(
+              x: 40.0,
+              y: self.loginButton.frame.size.height/2
+            )
+            self.spinner.alpha = 1.0
+        }, completion: nil)
+        
     }
     
     // MARK: UITextFieldDelegate
