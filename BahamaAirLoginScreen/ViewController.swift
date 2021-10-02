3551e1268c97476f589811f89e5933d635c0f63b
yamamoto <kyoyamamoto1202@gmail.com>
2021-10-02 16:05:58 +0900

    adding transaction

--- BahamaAirLoginScreen/ViewController.swift
+++ BahamaAirLoginScreen/ViewController.swift
@@ -73,6 +73,9 @@ class ViewController: UIViewController {
         label.textColor = UIColor(red: 0.89, green: 0.38, blue: 0.0, alpha: 1.0)
         label.textAlignment = .center
         status.addSubview(label)
+        
+        statusPosition = status.center
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -175,18 +178,20 @@ class ViewController: UIViewController {
     @IBAction func login() {
         view.endEditing(true)
         UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: {
+            self.loginButton.center.y += 60
             self.loginButton.bounds.size.width += 80.0
-            
             self.loginButton.backgroundColor =
             UIColor(red: 0.85, green: 0.83, blue: 0.45, alpha: 1.0)
             
             self.spinner.center = CGPoint(
-              x: 40.0,
-              y: self.loginButton.frame.size.height/2
+                x: 40.0,
+                y: self.loginButton.frame.size.height/2
             )
             self.spinner.alpha = 1.0
-        }, completion: nil)
-        
+        },completion: { _ in
+            self.showMessage(index: 0)
+            
+        })
     }
     
     // MARK: UITextFieldDelegate
@@ -196,5 +201,40 @@ class ViewController: UIViewController {
         nextField?.becomeFirstResponder()
         return true
     }
+    
+    func showMessage(index: Int) {
+        label.text = messages[index]
+        
+        UIView.transition(
+            with: status, duration: 0.33,
+            options: [.curveEaseOut, .transitionCurlDown],
+            animations: { self.status.isHidden = false },
+            completion: { _ in
+                delay(2.0) {
+                    if index < self.messages.count-1 {
+                        self.removeMessage(index: index)
+                    } else {
+                        //reset form
+                    }
+                }
+            }
+        )
+    }
+    
+    func removeMessage(index: Int) {
+      UIView.animate(withDuration: 0.33, delay: 0.0, options: [],
+        animations: {
+          self.status.center.x += self.view.frame.size.width
+        },
+        completion: { _ in
+          self.status.isHidden = true
+          self.status.center = self.statusPosition
+
+          self.showMessage(index: index+1)
+        }
+      )
+    }
+
+
     
 }
